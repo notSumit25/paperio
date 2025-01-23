b124986b89492c00c472b7383fd0c22a02b38250
Sumit <sumitgarg380@gmail.com>
2025-01-23 17:39:12 +0530

    asdas

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
-import GameCanvas from './components/GameCanvas';
+
 export default function Home() {
   return (
     <div>
-      <h1>Paper.io Clone</h1>
-      <GameCanvas />
       
     </div>
   );
